d886ce4699900f804ff106aff73362962799bca2
ChickenApps <JohnyLoots1@outlook.com>
2023-08-25 13:49:33 +0200

    Initial Upload

---
+++ Editor/OnebitStudioProjectSetup.cs
@@ -0,0 +1,122 @@
+using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using UnityEditor.PackageManager.Requests;
+using UnityEditor.PackageManager;
+
+public class QuickProjectSetup : UnityEditor.EditorWindow
+{
+    private string versionNumber = "1.1";
+    private List<Pack> packs;
+    static AddRequest Request;
+
+    public QuickProjectSetup(){
+        packs = new List<Pack>(){
+
+            new Pack("Generic", new List<PackageModule>(){
+                new PackageModule("com.unity.addressables", "Addressables"),
+                new PackageModule("com.unity.ai.navigation", "AI Navigation"),
+                new PackageModule("https://github.com/febucci/unitypackage-custom-hierarchy.git", "Febucci Custom Hierarchy"),
+                new PackageModule("com.unity.ide.visualstudio", "Visual Studio Editor")
+            }),
+
+            new Pack("2D Basics", new List<PackageModule>(){
+                new PackageModule("com.unity.2d.animation", "2D Animation"),
+                new PackageModule("com.unity.2d.pixel-perfect", "2D Pixel Perfect"),
+                new PackageModule("com.unity.2d.psdimporter", "2D PSD Importer"),
+                new PackageModule("com.unity.2d.sprite", "2D Sprite"),
+                new PackageModule("com.unity.2d.spriteshape", "2D SpriteShape"),
+                new PackageModule("com.unity.2d.tilemap", "2D Tilemap Editor"),
+                new PackageModule("com.unity.2d.tilemap.extras", "2D Tilemap Extras"),
+            }),
+
+            new Pack("C", new List<PackageModule>(){
+                new PackageModule("c", "c"),
+                new PackageModule("c", "c"),
+                new PackageModule("c", "c"),
+                new PackageModule("c", "c")
+            }),
+        };
+    }
+
+
+    [MenuItem("Tools/Onebit Studio Project Setup")]
+    static void Initilize()
+    {
+        //Window
+        QuickProjectSetup window = (QuickProjectSetup)EditorWindow.GetWindow(typeof(QuickProjectSetup), true, "My Empty Window");
+        var popup_Width = 180;
+        var popup_Height = 480;
+        window.position = new Rect(Screen.width, Screen.height - (popup_Height / 2), popup_Width, popup_Height);
+        window.Show();        
+    }
+
+
+    void OnGUI()
+    {
+        for (int i = 0; i < packs.Count; i++)
+        {
+            var module = packs[i];
+            module.importState = EditorGUILayout.BeginToggleGroup(module.pack_Name, module.importState);
+
+            for (int x = 0; x < module.packageModules.Count; x++)
+            {
+                module.packageModules[x].importState = EditorGUILayout.Toggle(module.packageModules[x].package_Name, module.packageModules[x].importState);
+            }
+            
+            EditorGUILayout.EndToggleGroup();
+        }
+
+        var buttonHeight = 25;
+
+        if (GUI.Button(new Rect(0, position.height - (buttonHeight * 1), position.width, buttonHeight), "Close Window!")){
+            this.Close();
+        }
+
+        if (GUI.Button(new Rect(0, position.height - (buttonHeight * 2), position.width, buttonHeight), "Import All")){
+            importAll();
+        }
+
+        if (GUI.Button(new Rect(0, position.height - (buttonHeight * 3), position.width, buttonHeight), "Clear All")){
+            this.Close();
+        }
+    }
+
+
+    void importAll(){
+        for (int i = 0; i < packs.Count; i++){
+            var module = packs[i];
+            for (int x = 0; x < module.packageModules.Count; x++)
+            {
+                Request = Client.Add(module.packageModules[x].packageURL);
+            }
+        }
+    }
+}
+
+public class Pack{
+    public string pack_Name;
+    public List<PackageModule> packageModules;
+    public bool importState = false;
+    public Pack(string package_Name, List<PackageModule> modules){
+        this.pack_Name = package_Name;
+        this.packageModules = modules;
+    }
+}
+
+public class PackageModule{
+
+    public PackageModule(string URL, string name){
+        this.packageURL = URL;
+        this.package_Name = name;
+    }
+    public bool importState = false;
+     public string packageURL;
+    public string package_Name;
+}
+
+
+
+
+
+    
